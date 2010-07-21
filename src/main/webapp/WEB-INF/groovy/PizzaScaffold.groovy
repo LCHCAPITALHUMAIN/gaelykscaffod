b0b589282f035abb7fd442bb640081380fc7d0f9
sammyrulez <sammyrulez@localhost>
2010-07-21 17:15:46 +0200

    removed src/main/webapp/WEB-INF/groovy/PizzaScaffold.groovy

--- src/main/webapp/WEB-INF/groovy/PizzaScaffold.groovy
+++
@@ -1,9 +0,0 @@
-
-import com.kyub.gaelyk.scaffold.*;
-import com.kyub.gaelyk.scaffold.meta.*;
-
-
-ScaffoldRegistar.registerEntity('pizza',new EntityDescriptor(
-								entityName : 'com.kyub.pizeria.model.Pizza',
-								entityStruct : ['name':new FieldDescriptor(mandatory:true),'price':new FieldDescriptor(typeName:'Double'),'description':new FieldDescriptor(lenght:1024)],
-								listProperties:['name','price'] , insertProperties:['name','price','description'] ,editProperties:['price','description'] ))
